03be6bbe321bd684c80dc3afa41beb68536964ab
mradovcic <radovcicmarko@gmail.com>
2024-05-09 13:07:37 +0200

    ...

--- OrleansSerializationTest.Models/StreamSettings.cs
+++ OrleansSerializationTest.Models/StreamSettings.cs
@@ -3,7 +3,7 @@
     public static class StreamSettings
     {
         public const string MEMORY_STREAM_PROVIDER = "MEMORY_STREAM_PROVIDER";
-        public const int MEMORY_STREAM_PUBLISH_INTERVAL = 10;
+        public const int MEMORY_STREAM_PUBLISH_INTERVAL = 1000;
         public const int MEMORY_STREAM_PARTITIONS_COUNT = 8;
         public const int MEMORY_STREAM_PRODUCER_DEACTIVATION_INTERVAL = 1000 * 60 * 60;
         public const string MEMORY_STREAM_NAMESPACE = "RANDOMDATA";
